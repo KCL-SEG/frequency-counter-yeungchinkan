8f405e09d1b21377f6088d8f4629ce203c992b62
Ken <y23@mail.com>
2022-09-27 17:51:16 +0100

    Use get

--- frequencies.py
+++ frequencies.py
@@ -1,16 +1,10 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-from curses.has_key import has_key
-
 
 def frequencies(items):
     frequencies = {}
     for i in items:
-        
-        if str(i) in frequencies == False and i in frequencies == False:
-            frequencies[str(i)] = 1
-        else:
-            frequencies[str(i)] += 1
-    # Your code goes here
+        frequencies[str(i)] = frequencies.get(str(i), 0) + 1
+
     return frequencies
