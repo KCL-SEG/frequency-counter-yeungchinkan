c2da8307da346b3e4c3e009ba31dded063c5c23a
Ken <y23@mail.com>
2022-09-27 17:38:32 +0100

    Correct code

--- frequencies.py
+++ frequencies.py
@@ -1,13 +1,16 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from curses.has_key import has_key
+
+
 def frequencies(items):
     frequencies = {}
     for i in items:
-        count = frequencies[i]
-        if count == None:
+        
+        if has_key(frequencies, i) == False:
             frequencies[i] = 1
         else:
-            frequencies[i] = count + 1
+            frequencies[i] += 1
     # Your code goes here
     return frequencies
