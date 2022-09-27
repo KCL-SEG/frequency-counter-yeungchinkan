aad990f636b7b6693b66a3f2c1cd56c6ab788edd
Ken <y23@mail.com>
2022-09-27 17:34:40 +0100

    return freqeuncies

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
+    for i in items:
+        count = frequencies[i]
+        if count == None:
+            frequencies[i] = 1
+        else:
+            frequencies[i] = count + 1
     # Your code goes here
     return frequencies
